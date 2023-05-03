36df0d52acc5321d19e8e5d0491a57cf44a1a43d
Fred p <108587738+Misterteefred@users.noreply.github.com>
2023-05-03 08:25:09 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,11 +1,3 @@
-/*
-REFACTORING :
-déjà fais : 
-elInput.addEventListener("keyup", backSpaceSearch);
-elInput2.addEventListener("keyup", backSpaceSearch);
-elDeleteSearch.addEventListener("click", clearSearch);
-elDeleteSearch2.addEventListener("click", clearSearch);
-*/
 /* ---SELECT HEADER--- */
 const elInput = document.querySelector(".bottomMenu input");
 const elInput2 = document.querySelector(".contentSecondaryInput input");
@@ -20,79 +12,51 @@ const elMenu = document.querySelector(".menu");
 const elHeader = document.querySelector(".header");
 const elLinks = document.querySelectorAll(".links > div > span");
 const elOpacityDiv = document.querySelector(".opacityDiv");
-
 /* --- SELECT THUMBNAILS --- */
 const elCards = document.querySelectorAll(".mediaCard");
-// console.log(elCards);
+
 /* LISTENER HEADER */
 elInput.addEventListener("click", clickSearch);
-elInput2.addEventListener("click", clickSearch2);
-
-/* * refactoring  ok * */
+elInput2.addEventListener("click", clickSearch);
 elInput.addEventListener("keyup", backSpaceSearch);
 elInput2.addEventListener("keyup", backSpaceSearch);
 elDeleteSearch.addEventListener("click", clearSearch);
 elDeleteSearch2.addEventListener("click", clearSearch);
-/* * --------------- * */
 elCroixSearch.addEventListener("click", closeSearch);
-elCroixSearch2.addEventListener("click", closeSearch2);
+elCroixSearch2.addEventListener("click", closeSearch);
 window.addEventListener("scroll", scrollFunction);
-window.addEventListener("load", () => {
-  elInput.style.backgroundImage = "url(./image/logos/menu/search.svg)";
-  elInput2.style.backgroundImage = "url(./image/logos/menu/search.svg)";
-});
+
 
 function clickSearch() {
-  elInput.classList.add("activeSearch");
-  elInput.classList.remove("searchColor");
-  elInput.style.backgroundImage = "none";
-  elInput.setAttribute("placeholder", "RPG, hack 'n' Slash, multijoueur...");
-  elCroixSearch.style.display = "block";
-}
-function clickSearch2() {
-  elInput2.classList.add("activeSearch2");
-  elInput2.style.backgroundImage = "none";
-  elInput2.setAttribute("placeholder", "RPG, hack 'n' Slash, multijoueur...");
-  elCroixSearch2.style.display = "block";
+  this.classList.add("activeSearch");
+  this.style.backgroundColor = '#ff5400';
+  this.style.backgroundImage = "none";
+  this.setAttribute("placeholder", "RPG, hack 'n' Slash, multijoueur...");
+  this.nextElementSibling.nextElementSibling.style.display = "block";
   if (window.matchMedia("(max-width: 650px)").matches) {
     elLogo = document.querySelector('.divLogo').parentNode;
     elIconRight = document.querySelector('.headerRight');
     elLogo.style.display = 'none';
     elIconRight.style.display = 'none';
-    elInput2.parentNode.style.marginLeft = "0"
-    elInput2.parentNode.style.marginRight = "20px";
+    this.parentNode.style.marginLeft = "0";
+    this.parentNode.style.marginRight = "20px";
   };
 }
 
-function closeSearch(e) {
-  elInput.removeAttribute("placeholder", "RPG, hack 'n' Slash, multijoueur...");
-  elInput.classList.remove("activeSearch");
-  elInput.classList.add("searchColor");
-  elCroixSearch.style.display = "none";
-  elDeleteSearch.style.display = "none";
-  elInput.addEventListener("transitionend", closeSearchTransitionEnd);
+function closeSearch() {
+  let input = this.previousElementSibling.previousElementSibling;
+  input.removeAttribute("placeholder", "RPG, hack 'n' Slash, multijoueur...");
+  input.removeAttribute("style", "background-color: rgb(255, 84, 0)");
+  input.classList.remove("activeSearch");
+  input.style.backgroundImage = "none";
+  this.style.display = "none";
+  this.previousElementSibling.style.display = "none";
+  input.addEventListener("transitionend", closeSearchTransitionEnd);
   function closeSearchTransitionEnd(e) {
     if (e.propertyName === "width") {
-      elInput.style.backgroundImage = "url(image/logos/menu/search.svg)";
-      elInput.removeEventListener("transitionend", closeSearchTransitionEnd);
-    }
-  }
-
-}
-function closeSearch2(e) {
-  elInput2.removeAttribute(
-    "placeholder",
-    "RPG, hack 'n' Slash, multijoueur..."
-  );
-  elInput2.classList.remove("activeSearch2");
-  // elInput2.classList.add("searchColor");
-  elCroixSearch2.style.display = "none";
-  elDeleteSearch2.style.display = "none";
-  elInput2.addEventListener("transitionend", closeSearchTransitionEnd);
-  function closeSearchTransitionEnd(e) {
-    if (e.propertyName === "width") {
-      elInput2.style.backgroundImage = "url(image/logos/menu/search.svg)";
-      elInput2.removeEventListener("transitionend", closeSearchTransitionEnd);
+      input.removeAttribute("style", "background-color: none;")
+      input.style.backgroundImage = "url(./image/logos/menu/search.svg)";
+      input.removeEventListener("transitionend", closeSearchTransitionEnd);
     }
   }
   if (window.matchMedia("(max-width: 650px)").matches) {
@@ -100,8 +64,8 @@ function closeSearch2(e) {
     elIconRight = document.querySelector('.headerRight');
     elLogo.style.display = 'block';
     elIconRight.style.display = 'flex';
-    elInput2.parentNode.style.marginLeft = "50px"
-    elInput2.parentNode.style.marginRight = "50px";
+    input.parentNode.style.marginLeft = "50px"
+    input.parentNode.style.marginRight = "50px";
   };
 }
 
@@ -120,6 +84,7 @@ function scrollFunction(e) {
       elMenu.style.display = "none";
       elOpacityDiv.style.display = "none";
     } else {
+      //la class menuDown pourrait ne pas exister : syntaxe "replace" impossible.
       elMenu.classList.remove("menuDown");
       elMenu.classList.add("menuUp");
       elHeader.classList.add("headerScrolled");
@@ -131,8 +96,7 @@ function scrollFunction(e) {
     if (window.matchMedia("(max-width: 950px)").matches) {
       elMenu.style.display = "block";
     } else {
-      elMenu.classList.remove("menuUp");
-      elMenu.classList.add("menuDown");
+      elMenu.classList.replace("menuUp", "menuDown");
       elHeader.classList.remove("headerScrolled");
       elInput.classList.remove("searchScrolled");
       setTimeout(() => {
@@ -144,14 +108,13 @@ function scrollFunction(e) {
 }
 
 /*   - SOUS MENU - */
-
 elLinks.forEach(function (link) {
   link.addEventListener("click", displayMenu);
-  function displayMenu(e) {
+  function displayMenu() {
     elOpacityDiv.style.display = "block";
-    let menuAffiche = e.target.nextElementSibling;
+    let menuAffiche = this.nextElementSibling;
     menuAffiche.style.display = "block";
-    let parent = e.target.parentNode;
+    let parent = this.parentNode;
 
     parent.addEventListener("mouseleave", function (event) {
       if (event) {
@@ -163,7 +126,6 @@ elLinks.forEach(function (link) {
 });
 
 /* - LISTENER THUMBNAILS */
-
 elCards.forEach(function (elCard) {
   let image = elCard.firstElementChild;
   let video = image.nextElementSibling;
@@ -175,58 +137,35 @@ elCards.forEach(function (elCard) {
   function mouseenter() {
     iconPromo.classList.add("fadeOut");
     if (window.matchMedia("(min-width: 801px)").matches) {
-      //"Derrière" l'image, La video est déjà zoomé en css, mais en opacity 0.
-      //J'ajoute  la class "scale_fadeOut" à mon image : elle va zoomer, et ensuite disparaitre au bout de 500ms (voir keyframes)
-      //je supprime la classe unScale car il est possible qu'elle soit déjà attribuée (en rentrant dans l'écouteur "mouseleave")
       image.classList.remove("unScale");
       image.classList.add("scale_fadeOut");
 
-
-      //Au moment ou mon image disparait, c'est ma video qui va apparaitre avec la classe "fadeIn" (voir keyframes), ça va lui prendre "300ms"
       timeoutVideo = setTimeout(displayedVideo, 300);
     } else {
-      //transform: scale(1.07);
       video.style.transform = "scale(1)";
       image.classList.add('fadeOut');
-
       timeoutVideo = setTimeout(displayedVideo, 300);
     }
-
   }
 
   elCard.addEventListener("mouseleave", mouseleave);
 
   function mouseleave() {
-    //je "clear" le timeout de la fonction variable "timeoutVideo".
-    //c'est indispensable de clear le TimeOut car si on survol la video, mais qu'instantanément on quitte la zone de survol, à ce moment on entre dans le "mouseleave", et en même temps le settimeout du mouseenter se déclanche. ce qui est facheux...
-    //supprime le stopDisplayVideo et survol la vignette à toute vitesse pour voir la différence.
     stopDisplayVideo();
     video.pause();
-    //si on sort de la vignette, mettre la video en pause, et on lui supprime la classe pour "nettoyer" les attribus      
     video.classList.remove("fadeIn");
-    iconPromo.classList.remove("fadeOut");
-    iconPromo.classList.add("fadeIn");
+    iconPromo.classList.replace("fadeOut", "fadeIn");
     if (window.matchMedia("(min-width: 801px)").matches) {
-
-
-
-      //on fait réapparaitre l'image en remettant son opacité à 1, et un scale identique à la video
       image.style.opacity = "1";
       image.style.tranform = "scale(1.07)";
 
-      //on supprime la classe scale_fadeout pour que les deux lignes ci-dessus prennent effet, autrement le style du keyframe garde le dessus
-      image.classList.remove("scale_fadeOut");
-
-      //et on dezoome l'image
-      image.classList.add("unScale");
-
+      image.classList.replace("scale_fadeOut", "unScale");
 
     } else {
       image.style.opacity = "1";
       image.classList.remove('fadeOut');
     }
-    //OPTIONNEL :
-    //Pour nettoyer les attributs HTML des class et style précédemment appliqué, mais une fois les animations terminées.
+
     image.addEventListener("animationend", animationendImage);
     function animationendImage(e) {
       if (e.animationName === "unScale") {
@@ -249,9 +188,7 @@ elCards.forEach(function (elCard) {
 });
 
 /*   GSAP    */
-
 const elBanner = document.querySelector(".banner");
-
 gsap.to(elBanner, {
   scrollTrigger: {
     trigger: "headerContainer",
@@ -259,14 +196,4 @@ gsap.to(elBanner, {
   },
   y: 200,
 });
-// gsap.to(elOpacityDiv, {
-//   scrollTrigger: {
-//     trigger: "headerContainer",
-//     scrub: true,
-//   },
-//   y: 200,
-// });
-
-
-
 
